5f30c3d365ba6458a2338e80dc51a7b7dd4357f9
vdevcode <thanhvumaidao1234567@gmail.com>
2022-07-10 16:40:23 +0700

    first

--- main.js
+++ main.js
@@ -38,3 +38,53 @@ var swiper = new Swiper(".trending-content", {
           },
         },
 });
+
+let timeDate = document.querySelector('.time-date')
+
+let timeNow = new Date().getHours()
+let Day = timeNow >= 5 && timeNow < 12
+          ? "<i class='bx bxs-sun date-sun' ></i> good morning" : timeNow >= 12 && timeNow < 18
+          ? "<i class='bx bxs-ghost date-afternoon' ></i> good afternoon" : "<i class='bx bxs-moon date-night'></i> good evening"
+timeDate.innerHTML = `<h1>${Day}</h1>`
+
+var loader = document.getElementById('loader-website')
+window.addEventListener('load',function(){
+    loader.style.display = 'none'
+})
+//fullscreen
+
+
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = "Tắt Fullscreen";
+    }
+    else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = "Bật Fullscreen";
+    }
+});
+
+//progress-bar ( scroll)
+
+document.addEventListener('DOMContentLoaded',() =>{
+ 
+    const scrollBar = document.getElementById('value-progress')
+    document.addEventListener('scroll', () =>{
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+    const scrollHeight = document.documentElement.scrollHeight
+    const clientHeight = document.documentElement.clientHeight
+    
+    const percentPage = Math.floor((scrollTop / (scrollHeight - clientHeight) *100))
+    scrollBar.style.width = percentPage + "%"
+    
+  })
+})
+
+
+
